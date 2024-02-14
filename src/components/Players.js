81291b02175fb87aab7ffcf18b610394d2ef9f03
karaneliab <eliabkaran7884@gmail.com>
2024-02-15 00:47:17 +0300

    added some styling to enhance display,pagination,and also  a form in UI to enable user add new player--- tha only issue is that there is no persistence and doesnt post at the backend lets check on it in the morning

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,111 +1,217 @@
 import React, { useState, useEffect } from "react";
-
+import ReactPaginate from "react-paginate";
+import { Formik, Field, Form } from "formik";
 
 export default function Players() {
-    const [players, setPlayers] = useState([]);
-    
-    
+  const [players, setPlayers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [playersPerPage] = useState(12);
 
-    useEffect(() => {
-        FetchPlayerDetails();
-    }, []);
+  const initialValues = {
+    strPlayer: "",
+    strSigning: "",
+    strNationality: "",
+    strThumb: "",
+    strStatus: "",
+  };
 
-    const FetchPlayerDetails = async () => {
-        const teams = ['Arsenal','Barcelona','Dortmund','Monaco','Napoli'];
+  useEffect(() => {
+    FetchPlayerDetails();
+  }, []);
 
-        try {
-            const teamPromises = teams.map(async (team) => {
-                const url = `https://www.thesportsdb.com/api/v1/json/60130162/searchplayers.php?t=${team}`;
-                const response = await fetch(url, {
-                    headers: {
-                        'Accept': 'application/json',
-                        'User-agent': 'learning app',
-                    }
-                });
-                const data = await response.json();
-                return data.player;
-            });
+  const FetchPlayerDetails = async () => {
+    const teams = ['Arsenal','Barcelona','Dortmund','Monaco','Napoli','Real Madrid','Manchester United'];
 
-            const teamPlayers = await Promise.all(teamPromises);
-
-            // Flatten the array of arrays into a single array 
-            // initializes the favourite count to zero
-            const allPlayers = teamPlayers.flat().map(player => ({ ...player, favoriteCount: 0 }));
-
-            setPlayers(allPlayers);
-            
-            
-        } catch (error) {
-            console.error('Error fetching data', error);
-        }
-    }
-
-    // const newPlayer= {
-    //     strPlayer: '',
-    //     strNumber: '',
-    //     strThumb: '',
-    //     strNationality: '',
-    //     strDescriptionEN:"",
-    //     strStatus:''
-    // }
-
-    // const addPlayer = async () => {
-    //     try {
-    //         const response = await fetch('http://localhost:8001/players', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json'
-    //             },
-    //             body: JSON.stringify(newPlayer)
-    //         })
-    //         if (response.ok) {
-    //             alert('Player added successfully');
-    //         } else {
-    //             console.error('Failed to add player')
-    //         }
-    //     } catch (error) {
-    //         console.error('Failed to add player', error);
-    //     }
-    //     setPlayers(prevPlayers => [...prevPlayers, newPlayer])
-    // }
-    
-    //  updates the favourite count by one of each player 
-    const handleFavourite = (index) => {
-        setPlayers(prevPlayers => {
-            const updatedPlayers = [...prevPlayers];
-            updatedPlayers[index].favoriteCount += 1;
-            return updatedPlayers;
+    try {
+      const teamPromises = teams.map(async (team) => {
+        const url = `https://www.thesportsdb.com/api/v1/json/60130162/searchplayers.php?t=${team}`;
+        const response = await fetch(url, {
+          headers: {
+            'Accept': 'application/json',
+            'User-agent': 'learning app',
+          },
         });
-    }
-    function changeBackground(e) {
-        e.target.style.background = 'red';
-        e.target.style.cursor  = ' pointer'
-      }
-      function changeColorOnLeave(e){
-        e.target.style.background = 'green';
-        e.target.style.cursor  = ' pointer'
-      }
-    return (
-        <div >
-            <button >Add New Player</button>
-            {players.map((player, index) => (
-                <div key={index} className="container-sm container-frame" style={{ cursor: 'pointer', marginBottom: '20px',border: '3px solid #008000', width: '50%'}}>
-                    <figure className="figure">
-                    <img src={player.strThumb}alt="Avatar ofplayers"  style={{ width: 100, height: 100 }} />
-                    <p><strong>Name:</strong> {player.strPlayer}</p>
-                    <p><strong>Nationality:</strong> {player.strNationality}</p>
-                    <p><strong>Description: </strong>{player.strDescriptionEN}</p>
-                    <p ><strong>Status:</strong> {player.strStatus}</p>
-     
- 
+        const data = await response.json();
+        return data.player;
+      });
 
-                    <button onMouseLeave={changeColorOnLeave}  onMouseOver ={changeBackground}onClick={() => handleFavourite(index)} className= 'btn btn-info'style={{ cursor: 'pointer' }}>
-                            Favourites ❤️{player.favoriteCount}
-                        </button>
-                    
-                    </figure>
-                </div>
-            ))}
+      const teamPlayers = await Promise.all(teamPromises);
+
+      const allPlayers = teamPlayers.flat().map(player => ({ ...player, favoriteCount: 0 }));
+
+      setPlayers(allPlayers);
+    } catch (error) {
+      console.error('Error fetching data', error);
+    }
+  };
+
+  const addPlayer = async (values) => {
+    try {
+      const response = await fetch('http://localhost:8001/players', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+  
+      if (!response.ok) {
+        console.error('Failed to add player. Server returned:', response.status, response.statusText);
+        alert("error")
+      } else {
+        alert('Player added successfully');
+      }
+    } catch (error) {
+      console.error('Failed to add player', error.message);
+    }
+    setPlayers((prevPlayers) => [...prevPlayers, values]);
+  };
+  
+
+
+  const handleFavourite = (playerId) => {
+    setPlayers((prevPlayers) =>
+      prevPlayers.map((player) => {
+        if (player.idPlayer === playerId) {
+          return { ...player, favoriteCount: player.favoriteCount + 1 };
+        }
+        return player;
+      })
+    );
+  };
+
+  const pageCount = Math.ceil(players.length / playersPerPage);
+
+  const currentPlayers = players.slice(
+    currentPage * playersPerPage,
+    (currentPage + 1) * playersPerPage
+  );
+
+  const handlePageClick = (event) => {
+    setCurrentPage(event.selected);
+  };
+
+  function changeBackground(e) {
+    e.target.style.background = 'red';
+    e.target.style.cursor = 'pointer';
+  }
+
+  function changeColorOnLeave(e) {
+    e.target.style.background = 'green';
+    e.target.style.cursor = 'pointer';
+  }
+
+  return (
+    <div className="container mt-5">
+      <Formik className="input-group mb-3" initialValues={initialValues} onSubmit={addPlayer}>
+        <Form className="mb-3">
+          <Field
+            type="text"
+            className="form-control form-control-lg" 
+            name="strPlayer"
+            placeholder="Player Name"
+          />
+          <Field
+            type="text"
+            className="form-control form-control-lg" 
+            name="strNationality"
+            placeholder="Player Nationality"
+          />
+          <Field
+            type="text"
+            className="form-control form-control-lg" 
+            name="strStatus"
+            placeholder="Player Status"
+          />
+          <Field
+            type="text"
+            className="form-control form-control-lg" 
+            name="strSigning"
+            placeholder="Signing"
+          />
+          <Field
+            type="url"
+            className="form-control form-control-lg" 
+            name="strThumb"
+            placeholder="Image URL"
+          />
+          <button type="submit">Add Player</button>
+        </Form>
+      </Formik>
+      <h2 className="mb-3" style={{ fontFamily: 'Times New Roman Times serif' }}>
+        Players Details
+      </h2>
+      <div className="row">
+        {currentPlayers.map((player) => (
+          <div
+            key={player.idPlayer}
+            className="col-md-3 mb-3 mr-3"
+            style={{ cursor: 'pointer', paddingRight: '2%' }}
+          >
+            <figure className="figure">
+              <img
+                src={player.strThumb}
+                alt="Avatar of players"
+                style={{ width: '100%', height: 'auto' }}
+              />
+              <p className="card-text">
+                <strong>Name:</strong> {player.strPlayer}
+              </p>
+              <p className="card-text">
+                <strong>Nationality:</strong> {player.strNationality}
+              </p>
+              <p className="card-text">
+                <strong>Description: </strong>
+                {player.strSigning}
+              </p>
+              <p className="card-text">
+                <strong>Status:</strong> {player.strStatus}
+              </p>
+              <button
+                onMouseLeave={changeColorOnLeave}
+                onMouseOver={changeBackground}
+                onClick={() => handleFavourite(player.idPlayer)}
+                className="btn btn-info"
+                style={{ cursor: 'pointer' }}
+              >
+                Favourites ❤️{player.favoriteCount}
+              </button>
+            </figure>
+          </div>
+        ))}
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+        />
+      </div>
+      <div className="container mt-5">
+        <h2 className="mb-4">Players Details</h2>
+        <div className="row"></div>
+        <div className="row mt-4">
+          <div className="col-6">
+            {currentPage > 0 && (
+              <button
+                className="btn btn-info"
+                onClick={() => setCurrentPage(currentPage - 1)}
+              >
+                Previous
+              </button>
+            )}
+          </div>
+          <div className="col-6 text-right">
+            {currentPage < Math.ceil(players.length / playersPerPage) - 1 && (
+              <button
+                className="btn btn-dark"
+                onClick={() => setCurrentPage(currentPage + 1)}
+              >
+                Next
+              </button>
+            )}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
